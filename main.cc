35b49a89abc23c19219c09b0643fb55364cd205d
John Cottrill Brockway <jcbrockw@uwaterloo.ca>
2015-12-03 17:57:45 -0500

    Main accepts save games

--- main.cc
+++ main.cc
@@ -5,81 +5,102 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
   cout << "Welcome to PP9K! By Scott Broeders and John Brockway, Fall 2015" << endl;
   string input;
-  while (1) {                     // cycling through games
+  bool gameOn = true;
+  while (gameOn) {                     // cycling through games
     char player = 'b';
     Board b;
-    while (1) {
-      cout << endl << "To start a new game, enter ''game x y'' where x and y are either " << endl;
-      cout << "''human'' or ''computer[1-4]''. To enter setup, type ''setup''" << endl;
-      cout << "To stop playing for right now, type ''quit''" << endl;
-      getline(cin, input);
-      istringstream str(input);
-      input = "";
-      str >> input;
-      if (input == "game") {
-        b.defSetup();
-        b.print();
-        player = 'w';
-        // human vs computer stuff
-        break;
+    if (argc == 0) {
+      while (1) {
+        cout << endl << "To start a new game, enter ''game x y'' where x and y are either " << endl;
+        cout << "''human'' or ''computer[1-4]''. To enter setup, type ''setup''" << endl;
+        cout << "To stop playing for right now, type ''quit''" << endl;
+        getline(cin, input);
+        istringstream str(input);
+        input = "";
+        str >> input;
+        if (input == "game") {
+          b.defSetup();
+          b.print();
+          player = 'w';
+          // human vs computer stuff
+          break;
+        }
+        else if (input == "setup") {
+          player = 'w';
+          b.print();
+          while (1) {
+            char addType;
+            getline (cin, input);
+            istringstream iss(input);
+            iss >> input;
+            if (input == "+") {
+              iss >> addType;
+              iss >> input;
+              bool validAdd = b.setPiece(addType, input);
+              if (validAdd) b.print();
+              else {
+                cout << "Not a valid add" << endl;
+              }
+            }
+            else if (input == "-") {
+              iss >> input;
+              bool validRem = b.setPiece(0, input);
+              if (validRem) b.print();
+              else {
+                cout << "Not a valid remove" << endl;
+              }
+            }
+            else if (input == "=") {
+              iss >> input;
+              if (input == "black") player = 'b';
+              else player = 'w';
+            }
+            else if (input == "done") {
+              bool pawns = b.checkPawnsSetup();
+              bool kings = b.checkKingsSetup();
+              if (!pawns) {
+                cout << "Pawns on last row are illegal" << endl;
+              }
+              else if (!kings) {
+                cout << "Need exactly one of each king" << endl;
+              }
+              else break;
+            }
+          }
+          break;
+        }
+        else if (input == "quit") {
+          gameOn = false;
+          break;
+        }
+        else {
+          cout << "Enter valid input" << endl;
+        }
       }
-      else if (input == "setup") {
-        player = 'w';
-        b.print();
-        while (1) {
-          char addType;
-          getline (cin, input);
-          istringstream iss(input);
-          iss >> input;
-          if (input == "+") {
-            iss >> addType;
-            iss >> input;
-            bool validAdd = b.setPiece(addType, input);
-            if (validAdd) b.print();
-            else {
-              cout << "Not a valid add" << endl;
-            }
-          }
-          else if (input == "-") {
-            iss >> input;
-            bool validRem = b.setPiece(0, input);
-            if (validRem) b.print();
-            else {
-              cout << "Not a valid remove" << endl;
-            }
-          }
-          else if (input == "=") {
-            iss >> input;
-            if (input == "black") player = 'b';
-            else player = 'w';
-          }
-          else if (input == "done") {
-            bool pawns = b.checkPawnsSetup();
-            bool kings = b.checkKingsSetup();
-            if (!pawns) {
-              cout << "Pawns on last row are illegal" << endl;
-            }
-            else if (!kings) {
-              cout << "Need exactly one of each king" << endl;
-            }
-            else break;
+      argc;
+    }
+    else {
+      string line;
+      ifstream save(argv[0]);
+      for (int i = 0 ; i < 8 ; i++) {
+        getline(save, line);
+        for (int j = 0 ; j < 8 ; j++) {
+          if (line[j] != '_') {
+            b.setPiece(addType, line[j]);
           }
         }
-        break;
       }
-      else if (input == "quit") break;
-      else {
-        cout << "Enter valid input" << endl;
-      }
+      getline(save, line);
+      player = line[0] - 'A' +'a';
+      argc = 0;
     }
-    while (1) {                      // cycling through turns
-cout << player << "'s Move: ";
+    while (gameOn) {                      // cycling through turns
+      cout << player << "'s Move: ";
       input = "";
       getline(cin, input);
-cout << input << endl;
       if (input == "resign") {
         if (player == 'w') {
           cout << "Black wins!" << endl;
@@ -92,9 +113,7 @@ cout << input << endl;
       string input2;
       istringstream ss(input);
       ss >> input2;
-cout << input2 << endl;
       if (input2 == "move") {
-cout << "moving" << endl;
         // if current player is computer {} else:
         string start;
         string end;
